539438f788974d9282c436eb61f422a484049379
Nalin Vaidya <nalinvaidya@gmail.com>
2020-06-19 11:32:09 +0700

    Added migrations for the user and created a controller for user.

---
+++ database/migrations/2020_06_18_123600_create_user_role_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRoleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_role', function (Blueprint $table) {
+			$table->id();
+			$table->string('name');
+		});
+		
+		DB::table('user_role')->insert(
+			array(
+				"name" => "customer"
+			)
+		);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_role');
+    }
+}
---
+++ database/migrations/2020_06_18_125722_create_user_billing_address_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserBillingAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_billing_address', function (Blueprint $table) {
+			$table->id();
+			$table->string('address_line_one');
+			$table->string('address_line_two');
+			$table->string('city');
+			$table->string('state');
+			$table->string('country');
+			$table->string('pincode');
+			$table->unsignedBigInteger('user_id');
+			$table->foreign('user_id')->references('id')->on('user_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_billing_address');
+    }
+}
